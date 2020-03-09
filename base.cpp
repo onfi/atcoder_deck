6da2d8fe9f829e2fc491ac7a4e07a0bd9595a80c
simodake <simodake@gmail.com>
2020-03-09 23:45:37 +0900

    first

---
+++ base.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+/*
+ * 
+ */
+#define REPS(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define REP(i, n) REPS(i, 0, n)
+#define RREP(i, n) REPS(i, 1, n + 1)
+#define DEPS(i, a, n) for (ll i = (a); i >= (ll)(n); --i)
+#define DEP(i, n) DEPS(i, n, 0)
+#define EACH(i, n) for (auto&& i : n)
+
+const int MOD = 1e9 + 7;
+int main() {
+    ios::sync_with_stdio(false);
+   
+   cin.tie(0);
+    int a;
+    
+
+    cin >> a;
+    cout << a;
+    return 0;
+}
