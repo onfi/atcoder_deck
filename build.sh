a63830bce62cc30c384919840690a28bebf1d4ef
simodake <simodake@gmail.com>
2020-03-10 23:19:18 +0900

    cache

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-g++ -std=gnu++1y -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o $SCRIPT_DIR/tmp/a.out $1
-cat test.txt | $SCRIPT_DIR/tmp/a.out
+SHA1=$(cat $1 | sha1sum | sed -r "s/ .+//")
+test -f $SCRIPT_DIR/tmp/$SHA1.out || g++ -std=gnu++1y -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o $SCRIPT_DIR/tmp/$SHA1.out $1
+cat test.txt | $SCRIPT_DIR/tmp/$SHA1.out | tee tmp/result.txt 
